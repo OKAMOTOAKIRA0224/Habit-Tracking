7284b310e49e862bd56bc5e314ba1a75767ee12e
akira <0kinakomoti0@example.com>
2026-01-19 23:53:20 +0900

    テーブルの追加とデザイン変更

--- flaskr/db.py
+++ flaskr/db.py
@@ -2,5 +2,7 @@ import sqlite3
 DATABASE='database.db'
 def create_training_table():
     con=sqlite3.connect(DATABASE)
-    con.execute("CREATE TABLE IF NOT EXISTS training(day,title,kaisuu)")
+    con.execute("CREATE TABLE IF NOT EXISTS training(day TEXT, title TEXT, kaisuu TEXT, category TEXT)")
     con.close()
+if __name__ == "__main__":
+    create_training_table()
--- flaskr/main.py
+++ flaskr/main.py
@@ -1,38 +1,51 @@
-from flaskr import app
-from flask import render_template,request,redirect,url_for
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
-DATABASE='database.db'
+
+app = Flask(__name__)
+DATABASE = 'database.db'
+
 
 @app.route('/')
 def index():
-    con=sqlite3.connect(DATABASE)
-    db_training=con.execute("SELECT * FROM training").fetchall()
+    con = sqlite3.connect(DATABASE)
+    db_training = con.execute("SELECT * FROM training").fetchall()
     con.close()
 
-
-    training= []
+    training = []
     for row in db_training:
-        training.append({'day':row[0],"title":row[1],"kaisuu":row[2]})
-    
+        training.append({
+            'day': row[0],
+            'title': row[1],
+            'kaisuu': row[2],
+            'category': row[3]
+        })
 
-    return render_template(
-        'index.html',
-        training=training
-    )
+    return render_template('index.html', training=training)
+
 
 @app.route('/form')
 def form():
-    return render_template(
-        'form.html'
-    )
-@app.route('/register',methods=['POST'])
-def register():
-    day=request.form["day"]
-    title=request.form["title"]
-    kaisuu=request.form["kaisuu"]
+    return render_template('form.html')
 
-    con=sqlite3.connect(DATABASE)
-    con.execute("INSERT INTO training VALUES(?,?,?)",[day,title,kaisuu])
+
+@app.route('/register', methods=['POST'])
+def register():
+    category = request.form["category"]
+    day = request.form["day"]
+    title = request.form["title"]
+    kaisuu = request.form["kaisuu"]
+
+    con = sqlite3.connect(DATABASE)
+    con.execute(
+        "INSERT INTO training VALUES(?,?,?,?)",
+        [day, title, kaisuu, category]
+    )
     con.commit()
     con.close()
+
     return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
